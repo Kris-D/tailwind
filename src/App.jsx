e6bb3c6c2d397e6e19a7416c0a6255811a5281f8
Christopher Isioma Dunkwu <chriscjd001@gmail.com>
2023-08-17 00:46:05 +0100

    setup vite and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className="text-3xl font-bold underline">Welcome Kris to Vite</h1>
+     
+    </>
+  )
+}
+
+export default App
